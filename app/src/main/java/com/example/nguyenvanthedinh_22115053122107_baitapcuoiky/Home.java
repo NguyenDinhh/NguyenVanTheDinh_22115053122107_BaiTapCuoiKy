9cfbb29a89258586dd009a744ef93801045e0463
NguyenDinhh <dinhhandsome12a1@gmail.com>
2024-11-29 08:20:39 +0700

    Initial commit

---
+++ app/src/main/java/com/example/nguyenvanthedinh_22115053122107_baitapcuoiky/Home.java
@@ -0,0 +1,19 @@
+package com.example.nguyenvanthedinh_22115053122107_baitapcuoiky;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class Home extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+    }
+}
